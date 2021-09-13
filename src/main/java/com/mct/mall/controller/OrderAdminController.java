3959dee5de8abe4e01b0573da6e05e71b1122856
konoha1216 <konoha3219@gmail.com>
2021-09-13 23:05:35 +0800

    fix: deliver and finish method should be POST

--- src/main/java/com/mct/mall/controller/OrderAdminController.java
+++ src/main/java/com/mct/mall/controller/OrderAdminController.java
@@ -6,6 +6,7 @@ import com.mct.mall.service.OrderService;
 import io.swagger.annotations.ApiOperation;
 import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,15 +34,15 @@ public class OrderAdminController {
      * 0:canceled 10:not paid 20:paid 30:delivered 40:finished
      * @return
      */
-    @GetMapping("admin/order/delivered")
+    @PostMapping("admin/order/delivered")
     @ApiOperation("admin deliver a order")
     public ApiRestResponse delivered(String orderNo) {
         orderService.delivered(orderNo);
         return ApiRestResponse.success();
     }
 
-    @GetMapping("admin/order/finished")
-    @ApiOperation("admin/customer finish a order")
+    @PostMapping("/order/finish")
+    @ApiOperation("admin or customer finish a order")
     public ApiRestResponse finish(String orderNo) {
         orderService.finish(orderNo);
         return ApiRestResponse.success();
